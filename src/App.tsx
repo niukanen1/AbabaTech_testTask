74b27beff1a1fb1f139f6bf07edb6915e9634c23
Ivan Niukanen <ivanniu2003@gmail.com>
2023-01-16 01:14:48 +0200

    Home page, app.tsx fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 
 import './App.css';
 import Button from "./components/Buttons/Button/Button";
-import BubbleBackground from "./components/BubbleBackground/BubbleBackground";
 import LikeButton from "./components/Buttons/LikeButton/LikeButton";
 import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
@@ -10,11 +9,6 @@ import { FullMovieList } from './pages/FullMovieList/FullMovieList';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
-    const p : PersonType = {
-        id : 66633,
-        whoIs : "Some Role",
-        name : "Some Name"
-    }
   return (
     <div className="App">
         <Routes>
@@ -25,14 +19,6 @@ function App() {
                 </Route>
             </Route>
         </Routes>
-        <Header></Header>
-        <Button action={() => console.log("action")} >Some text</Button>
-        <LikeButton action={() => console.log("like")}/>
-        <div>
-            <PersonCard Person={p}/>
-            <PersonCard Person={p}/>
-        </div>
-
     </div>
   );
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,21 @@
 import { observer } from "mobx-react-lite";
+import styles from "./HomePage.module.css";
 
-function HomePage() { 
-    return ( 
-        <main>
-            <h1>Welcome to Movie point</h1>
-            <p>This website is meant to help you browse through millions of movies with no effort</p>
-            <h2>Start using it right now</h2>
-            <p>To start using Movies point you just need to sign up or log in with button in the top right corner</p>
-            <h2>Authors</h2>
-            <p>Movies point is developed by two developers</p>
-        </main>
-    )
+function HomePage() {
+	return (
+		<main className={styles.container}>
+			<article>
+				<h1>Welcome to Movie point</h1>
+				<p>This website is meant to help you browse through millions of movies with no effort</p>
+				<h2>Start using it right now</h2>
+				<p>
+					To start using Movies point you just need to sign up or log in with button in the top right corner
+				</p>
+				<h2>Authors</h2>
+				<p>Movies point is developed by two developers</p>
+			</article>
+		</main>
+	);
 }
 
-export default observer(HomePage)
+export default observer(HomePage);
